c410593433c2ec7af9acc13c8111f3ea604845b8
denki307 <mbtdenki@gmail.com>
2025-11-24 06:31:12 +0530

    Implement CricketGame class with scoring logic
    Added CricketGame class to manage game logic and scoring.

--- ASRC/game.py
+++ ASRC/game.py
@@ -1,1 +1,13 @@
-def play(): return "run" 
+import random
+
+class CricketGame:
+    def __init__(self):
+        self.score = 0
+
+    def play(self, user_run):
+        bot_run = random.randint(1, 6)
+        if bot_run == user_run:
+            return bot_run, True, self.score  # OUT
+        else:
+            self.score += user_run
+            return bot_run, False, self.score
